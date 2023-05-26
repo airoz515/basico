030d4d0bb3575bc5cbb3d1e5508626ca8a453561
sergio <airoz515sergio@gmail.com>
2023-05-26 17:55:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*variables en c# */
+string nombre = "panchito";
+int edad = 45;
+double pi = 3.1416;
+double decimalMuyGrande = 15.14324123;
+string [] frutas ={"manzana","platano","pera"};
+int [] edades = {1,2,3,4};
+
+
+
+
+
+Console.WriteLine(frutas[2]);
